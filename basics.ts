b44a81b2cd4c0cc23352aa3dff84ad1292a83fe2
nimroddanielmaayan <nimrodmayan@gmail.com>
2023-05-07 20:37:22 +0300

    Initial commit

---
+++ basics.ts
@@ -0,0 +1,41 @@
+// Primitives:
+
+let age: number;
+
+age = 12.1;
+
+let userName: string;
+
+userName = 'Max';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// Complex types:
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Max',
+  age: 32,
+};
+
+let peopleArray: {
+  name: string;
+  age: number;
+};
+[];
+
+// Any type (the default type):
+
+let anything: any;
+
+anything = 'Something';
